142b411e580a749b81a5c07551de15a8b2221c67
Greg Walden <talk@gsw.nyc>
2017-03-07 18:34:35 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+if (!process.env.TELEGRAM_TOKEN) throw new Error('Missing TELEGRAM_TOKEN');
+
+const Bot = require('node-telegram-bot')
+const yahooFinance = require('yahoo-finance')
+
+function messageHandler(msg) {
+  if (!msg.text) return;
+
+  const match = msg.text.toUpperCase().match(/!QUOTE\s+([A-Z]+)/i);
+  if (!match) return;
+
+  return yahooFinance.snapshot({
+    symbol: match[1],
+    fields: ['l1', 'n']
+  }).then(quote => {
+    bot.sendMessage({
+      chat_id: msg.chat.id,
+      text: `${quote.name.trim()}: $${quote.lastTradePriceOnly.toFixed(2)}`
+    })
+  })
+}
+
+const bot = new Bot({
+  token: process.env.TELEGRAM_TOKEN
+}).on('message', messageHandler).start();
